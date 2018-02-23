b601853c67c577584a6994e5563fa47267625cf6
Zac <hiremytech@gmail.com>
2018-02-23 13:08:50 -0700

    Initial commit

---
+++ src/com/zachary/gq/l5/Main.java
@@ -0,0 +1,11 @@
+package com.zachary.gq.l5;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hello World again!");
+        
+    }
+}
